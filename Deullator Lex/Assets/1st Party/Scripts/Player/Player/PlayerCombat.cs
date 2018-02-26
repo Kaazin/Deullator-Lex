0e818cba3731e9b4dd36e5e3142c8aa76e130ac7
Kaaz!n <kaazin.splatoon@gmail.com>
2018-02-25 19:02:02 -0500

    eoeuuanfsdinfdsbfi;v

---
+++ Deullator Lex/Assets/1st Party/Scripts/Player/Player/PlayerCombat.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+	Animator anim;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Punch Wk"))
+			anim.SetBool ("Punch Wk", true);
+		else
+			anim.SetBool ("Punch Wk", false);
+
+		if (Input.GetButtonDown ("Punch Str"))
+			anim.SetBool ("Punch Str", true);
+		else
+			anim.SetBool ("Punch Str", false);
+
+		if (Input.GetButtonDown ("Kick Wk"))
+			anim.SetBool ("Kick Wk", true);
+		else
+			anim.SetBool ("Kick Wk", false);
+
+		if (Input.GetButtonDown ("Kick Str"))
+			anim.SetBool ("Kick Str", true);
+		else
+			anim.SetBool ("Kick Str", false);
+		
+	}
+
+
+
+
+}
