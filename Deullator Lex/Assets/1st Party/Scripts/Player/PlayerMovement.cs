71f03efb9bf9286b83de54e7ec58984b9c997de5
Kaaz!n <kaazin.splatoon@gmail.com>
2018-02-25 20:18:50 -0500

    commented player movment

--- Deullator Lex/Assets/1st Party/Scripts/Player/PlayerMovement.cs
+++ Deullator Lex/Assets/1st Party/Scripts/Player/PlayerMovement.cs
@@ -10,23 +10,25 @@ public class PlayerMovement : MonoBehaviour
 	//PlayerJump playerJump;
 	Rigidbody rb;
 	//EnemyHealth enemyHealth;
-	public bool IsPlayer1;
-	public float warpDistance = 5;
-	public AudioSource warpSound;
-	public GameObject mesh;
-	Renderer[] rend;
-	public GameObject warpEffect;
-	Animator anim;
-	public  Vector3 dir;
-	public Vector3 pos;
-	Transform p2;
+	public bool IsPlayer1;	//is the player attached to the script player 1
+	public float warpDistance = 5; //the distance the player will travel when warping
+	public AudioSource warpSound;	//the waarp sound effect
+	public GameObject mesh;			//the character itself
+	Renderer[] rend;				//the renderers on the character
+	public GameObject warpEffect; //the warp visula effect
+	Animator anim;					//the character animator
+	public  Vector3 dir;			//the character's movement direction
+	public Vector3 pos;				//the character's position
+	Transform p2;					//player 2's transform
 	//BoxCollider BodyCol;
 	//CharacterController controller;
-	EnergyBar energyBar;
+	EnergyBar energyBar;			//the energy bar script attache to the player
 
 
 	void Awake ()
 	{
+		//set up the references
+
 		//controller = GetComponent<CharacterController> ();
 		rb = GetComponent<Rigidbody> ();
 
@@ -37,18 +39,25 @@ public class PlayerMovement : MonoBehaviour
 	}
 	void Update ()
 	{
+		//fix the player's position on the Z axis
 		transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+
+		//declare variables for movemnt on axes
 		float moveH = Input.GetAxis ("Horizontal") * speed;
 		float moveV = Input.GetAxis ("Vertical") * speed;
 
 
-
-		dir = new Vector3 (moveH, 0, 0);
+		//assign a value to dir
+		dir = new Vector3 (moveH, dir.y, 0);
 		//transform.Translate (dir * Time.deltaTime);
 
+		//add velocity to the rigidbody
 		rb.velocity = dir;
 	
 		//controller.Move (dir * Time.deltaTime);
+
+
+		//if there is horizotnal movement on the stick we should play the walking animation
 
 		if (moveH > 0)
 			anim.SetBool ("WalkF", true);
@@ -59,6 +68,8 @@ public class PlayerMovement : MonoBehaviour
 		else
 			anim.SetBool ("WalkB", false);
 
+
+		//if the warp button is pressed and we have enough energy we should warp
 		if (Input.GetButtonDown ("Warp") && energyBar.currentValue >=  100/3)
 			Warp ();
 
@@ -68,27 +79,45 @@ public class PlayerMovement : MonoBehaviour
 
 void Warp()
 	{
+		//take away some energy
 		energyBar.UseEnergy (100/ 3);
 
+		//disbale box colliders so we don't bump into anythng while warping
 		GetComponent<BoxCollider> ().isTrigger = false;
+
+		//disbale the mesh gameobject so we "diappear"
 		mesh.SetActive (false);
+
+		//move during the warp
 		transform.position = transform.position + new Vector3 (Input.GetAxisRaw ("Horizontal") * warpDistance, Input.GetAxisRaw ("Vertical") * warpDistance, 0);
+
+		//play the warp sound
 		warpSound.PlayOneShot (warpSound.clip);
 
+		//enable the warp effect
 		warpEffect.SetActive (true);
+
+		//begin to appear
 		StartCoroutine (Appear());
 	}
 
 
 		IEnumerator Appear()
 		{
+		//disable the warp effect
 		warpEffect.SetActive (false);
+		//wait for a thenth of a second
 		yield return new WaitForSeconds (.1f);
+		//play the waarp effect again
 		warpEffect.SetActive (true);
+		//enable the mesh
 		mesh.SetActive (true);
-		warpEffect.SetActive (true);
+		//disbale the warp effect
+		warpEffect.SetActive (false);
+		//stop the player's movement
 		transform.position = transform.position;
 		//rb.velocity = dir;
+		//reenable collisions
 		GetComponent<BoxCollider> ().isTrigger = true;
 		}
 
