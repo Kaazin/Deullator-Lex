082de422b8419d9711d65520e5de7b1cb0a71e3b
Kaaz!n <kaazin.splatoon@gmail.com>
2018-02-26 13:57:02 -0500

    Created starting countdown for the fight

--- Deullator Lex/Assets/1st Party/Scripts/Misc/UIScript.cs
+++ Deullator Lex/Assets/1st Party/Scripts/Misc/UIScript.cs
@@ -6,21 +6,38 @@ using UnityEngine.UI;
 public class UIScript : MonoBehaviour
 {
 	//you had timeLeft as the name of both the int and text variable and so I put an underscore on the int one to take away the error :)
+
     public GameObject youwin;
 
 	//in order to subtract Time.deltaTime your variable needs to be a float
-	public float _timeLeft = 300f;
+	public float _timeLeft = 99f;
+
+	float roundTimeLeft;
 
 	public Text timeLeft;
 
-    void Update() {
-        _timeLeft -= Time.deltaTime;
-        timeLeft.text = "300" + timeLeft;
+	public float startDelay; //the delay for the timer to start
+	public float delayTimer;// how much time has passed befor the delay has started
+
+    void Update() 
+	{
+		roundTimeLeft = Mathf.Round (_timeLeft);
+		//start the delay timer
+		delayTimer += Time.deltaTime;
+		//if the delay timer has exceeded the start delay
+		if (delayTimer >= startDelay && _timeLeft > 0) 
+		{
+			
+			//start the timer
+			_timeLeft -= Time.deltaTime;
+		
+			//set the time string to the current time left
+			timeLeft.text = roundTimeLeft.ToString ();
+		} 
+
+
 	}
-	
-    
-	
- }
+}
     
  
 
