6b40aab2707d2e8f0c027832451868069c7dcf2a
Smasson76 <Smasson1998@gmail.com>
2018-02-25 21:26:31 -0600

    Added Teleport Sound, added New scripts, added health and stamina bar, added timer and its script(needs fixing)

---
+++ Deullator Lex/Assets/1st Party/Scripts/Misc/UIScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+
+    public GameObject youwin;
+    public int timeLeft = 300f;
+
+	public text timeLeft
+
+    void Update() {
+        timeLeft -= Time.deltaTime;
+        timeLeft.text = "300" + timeLeft;
+	}
+	
+    
+	
+ }
+    
+ 
+
+
+
--- Deullator Lex/Assets/1st Party/Scripts/Player/EnergyBar.cs
+++ Deullator Lex/Assets/1st Party/Scripts/Player/EnergyBar.cs
@@ -14,6 +14,7 @@ public class EnergyBar : MonoBehaviour
 	float timer;
 
 
+
 	Slider energyBar;
 	public bool isPlayer1 = true; 
 	void Awake()
