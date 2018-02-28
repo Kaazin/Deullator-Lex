7e9b272abd4b40df42925029725c675aea23f524
Kaaz!n <kaazin.splatoon@gmail.com>
2018-02-28 13:59:40 -0500

    Created Player Health Script

---
+++ Deullator Lex/Assets/1st Party/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerHealth : MonoBehaviour
+{
+	public float maxHealth = 100f;
+	public float currentHealth;
+	public float time;
+	Slider[] healthBar;	//the main health bar
+
+	public bool takeDamage;
+
+	void Awake ()
+	{
+		currentHealth = maxHealth;
+
+		//set up the references
+		if(GetComponent<PlayerMovement>().IsPlayer1)
+		healthBar = GameObject.Find("p1HealthBar").GetComponentsInChildren<Slider>();
+		else
+		healthBar = GameObject.Find("p2HealthBar").GetComponentsInChildren<Slider>();
+
+
+	}
+		
+	void TakeDamage(float amount)
+	{
+		currentHealth -= amount;
+
+		healthBar [1].value = currentHealth;
+
+		StopCoroutine (ResidualHealth ());
+		StartCoroutine (ResidualHealth ());
+
+		if (currentHealth <= 0) 
+		{
+			Death ();
+		}
+		
+	}
+
+	void Death()
+	{
+		Debug.Log (this.gameObject.name + " " + "Dead");
+	}
+
+	IEnumerator ResidualHealth()
+	{
+		yield return new WaitForSeconds (3);
+		healthBar [0].value = Mathf.Lerp (healthBar [0].value, currentHealth, Time.deltaTime * time);
+	}
+
+}
+
