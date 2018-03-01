62b0bd4f5382c0d11b751bc7e6948b8681864d6a
Kaazin <kaazin.splatoon@gmail.com>
2018-03-01 11:25:40 -0500

    Started Reconrfiguring aniamtor controller setup

--- Deullator Lex/Assets/1st Party/Scripts/Misc/DetectHit.cs
+++ Deullator Lex/Assets/1st Party/Scripts/Misc/DetectHit.cs
@@ -6,11 +6,17 @@ public class DetectHit : MonoBehaviour
 {
 	public float amount;
 
+	public string name;
+		
 	void OnTriggerEnter(Collider col)
 	{
 		PlayerHealth playerHealth = col.GetComponent<PlayerHealth> ();
+		if (playerHealth != null) 
+		{
+			Debug.Log ("Hit");
+			Debug.Log (col.transform.root.name);
+			playerHealth.TakeDamage (amount);
 
-		if (playerHealth != null)
-		playerHealth.TakeDamage(amount);
+		}
 	}
 }
